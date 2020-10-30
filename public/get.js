d698dcf58baae3252972e571b87b38b3a7748921
Don Blair <donblair@gmail.com>
2020-10-30 19:28:45 -0400

    update

--- public/get.js
+++ public/get.js
@@ -39,19 +39,8 @@
 
     //var mymap = L.map('mapid').setView([myJson[0].lat,myJson[0].lon], 11);
 
-	var mymap = L.map('mapid');
 
-	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-		maxZoom: 18,
-		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-		id: 'mapbox/streets-v11',
-		tileSize: 512,
-		zoomOffset: -1
-	}).addTo(mymap);
-
-	mymap.setView(new L.LatLng(ave_lat, ave_lon), 11);
+	mymap.setView(new L.LatLng(ave_lat, ave_lon), 10);
 
 	//mymap.fitBounds(bounds);
 
